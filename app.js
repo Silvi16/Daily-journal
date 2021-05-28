cd60a215f537873500511a0afa0879d9ba4f00e9
Silvia Krajnakova <silvia.krajnakova@gmail.com>
2021-05-28 16:02:03 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const ejs = require('ejs')
+
+const app = express()
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+
+app.get('/', (req, res) => {
+    
+    res.render('index')
+})
+
+
+
+
+
+
+
+
+
+
+app.listen(8000, () => {
+    console.log('Listening on port 8000')
+})
